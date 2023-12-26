63d5f2f02b839433a59af261af5300aaf33f1334
hninoowailwin <hninoowaiucsy@gmail.com>
2023-12-26 14:16:59 +0630

    to-do-crud-with-postgresql-express

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Express-Postgresql
+## Project Setup
+
+```sh
+npm install
+```
+
+## Project Run on localhost
+
+```sh
+nodemon index.js
+```
+
+## pgAdmin Download and Setup
+
+```sh
+[pgAdmin Link](https://www.pgadmin.org/download/)
+```
--- index.js
+++ index.js
@@ -26,15 +26,40 @@ app.get("/", async (req, res) => {
   });
 });
 
-app.post("/add", (req, res) => {
+app.post("/add", async(req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
-  res.redirect("/");
+  try {
+    await dbConnect.query("INSERT INTO items (title) VALUES ($1)",[item]);
+    console.log("New item : ",item);
+    items.push({ title: item });
+    res.redirect("/");
+  } catch (error) {
+    console.log(error)
+  }
+  
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", async(req, res) => {
+  const item = req.body.updatedItemTitle;
+  const id = req.body.updatedItemId;
+  
+  try {
+    await dbConnect.query("UPDATE items SET title = ($1) WHERE id = $2" , [item, id]);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error)
+  }
+});
 
-app.post("/delete", (req, res) => {});
+app.post("/delete", async(req, res) => {
+  const id = req.body.deleteItemId;
+  try {
+    await dbConnect.query("DELETE FROM items WHERE id = $1", [id]);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error)
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
